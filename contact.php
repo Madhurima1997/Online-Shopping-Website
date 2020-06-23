0c35e890a841bb47819fb863e33ee3e337b1b5c3
Madhurima Chakraborty <61430343+Madhurima1997@users.noreply.github.com>
2020-06-23 12:42:48 +0530

    Add files via upload

---
+++ contact.php
@@ -0,0 +1,51 @@
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<link rel="stylesheet" type="text/css" href="contact.css">
+	</head>
+	<body>
+	<!-- Header -->
+		<header class="top-logo s12 s-12">
+			<img src="logo.png" alt="Z shopping store" height="100px" width="100px">
+			<h1>Z SHOPPING STORE</h1>
+		</header>
+		<!-- Header image banner -->
+		<section class="header-img s12 s-12">
+			<img src="contact.jpg">
+		</section>
+		<!-- Contact informations -->
+		<div class="info-contain s12 s-12">
+			<article class="info s4 s-12">
+				<img src="call.jpg" width="50px" height="50px"></br>
+				Contact number: +91-9999888877 
+			</article>
+			<article class="info s4 s-12">
+				<img src="loc.jpg" width="50px" height="50px"></br>
+				Location: Vellore Institute of Technology.
+			</article>
+			<article class="info s4 s-12">
+				<img src="email.jpg"></br>
+				Email: support@zstore.in
+			</article>
+		</div>
+		<!-- Footer -->
+		<section class="end s12 s-12">
+			<div class="foot-logo">
+				<img src="logo.png" alt="Z shopping store footer logo" height="50px" width="50px">
+				<h3>Z Shopping Store</h3>
+			</div>
+			<article class="link-list">
+				<ul>
+					<li><a href="about.php" class="divi">About</a></li>
+					<li><a href="contact.php" class="divi">Contact Us</a></li>
+					<li><a href="privacy.php" class="no-divi">Privacy Policy</a></li>
+				</ul>
+			</article>
+			<article class="copyr">
+				&copy 2020 ZShopping Store
+			</article>
+		</section>
+	</body>
+</html>
