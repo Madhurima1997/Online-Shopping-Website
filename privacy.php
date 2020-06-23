655f5aa4298be58c244369358b78a6f6aaffd1cc
Madhurima Chakraborty <61430343+Madhurima1997@users.noreply.github.com>
2020-06-23 12:45:25 +0530

    Add files via upload

---
+++ privacy.php
@@ -0,0 +1,41 @@
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta http-equiv="X-UA-Compatible" content="ie=edge">
+		<link rel="stylesheet" type="text/css" href="privacy.css">
+	</head>
+	<body>
+		<header class="top-logo s-12 s12">
+			<img src="logo.png" alt="Z shopping store" height="100px" width="100px">
+			<h1>Z SHOPPING STORE</h1>
+		</header>
+		<div class="s-12">
+			<h4> PRIVACY POLICY</h4>
+			<article>
+				<b>What information do we collect?</b>
+				<ul>
+					<li>We collect information from you when you register on the site, place an order, enter a contest or sweepstakes, respond to a survey or communication such as e-mail, or participate in another site feature.</li>
+					<li>When ordering or registering, we may ask you for your name, e-mail address, mailing address, phone number, credit card information or other information. You may, however, visit our site anonymously.</li>
+					<li>We also collect information about gift recipients so that we can fulfill the gift purchase. The information we collect about gift recipients is not used for marketing purposes.</li>
+					<li>Like many websites, we use "cookies" to enhance your experience and gather information about visitors and visits to our websites. Please refer to the "Do we use 'cookies'?" section below for information about cookies and how we use them.
+</li>
+				</ul>
+			</article>
+			<article>
+				<b>How do we use your information?</b>
+				We may use the information we collect from you when you register, purchase products, enter a contest or promotion, respond to a survey or marketing communication, surf the website, or use certain other site features in the following ways:
+				<ul>
+					<li>To personalize your site experience and to allow us to deliver the type of content and product offerings in which you are most interested.</li>
+					<li>To allow us to better service you in responding to your customer service requests.</li>
+					<li>To quickly process your transactions.</li>
+					<li>To administer a contest, promotion, survey or other site feature.</li>
+				</ul>
+			</article>
+			<article>
+				<b>How do we protect visitor information?</b>
+				We implement a variety of security measures to maintain the safety of your personal information. Your personal information is contained behind secured networks and is only accessible by a limited number of persons who have special access rights to such systems, and are required to keep the information confidential. When you place orders or access your personal information, we offer the use of a secure server. All sensitive/credit information you supply is transmitted via Secure Socket Layer (SSL) technology and then encrypted into our databases to be only accessed as stated above.
+			</article>
+		</div>
+	</body>
+</html>
